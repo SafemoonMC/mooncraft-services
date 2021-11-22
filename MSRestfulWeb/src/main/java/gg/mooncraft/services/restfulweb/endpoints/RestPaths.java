0da1ba7bd2b995a1b61bb16713a41bce27242f0e
Eduard Wayland <eduproard@gmail.com>
2021-11-22 22:01:30 +0100

    Added base VerifyWallet and VerifyDiscord handlers

--- MSRestfulWeb/src/main/java/gg/mooncraft/services/restfulweb/endpoints/RestPaths.java
+++ MSRestfulWeb/src/main/java/gg/mooncraft/services/restfulweb/endpoints/RestPaths.java
@@ -1,5 +1,7 @@
 package gg.mooncraft.services.restfulweb.endpoints;
 
+import gg.mooncraft.services.restfulweb.endpoints.post.impl.VerifyDiscord;
+import gg.mooncraft.services.restfulweb.endpoints.post.impl.VerifyWallet;
 import io.javalin.http.Handler;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
@@ -11,8 +13,8 @@ public enum RestPaths {
     PLAYER("/player/{player}", RequestType.GET, null),
     SERVERS("/servers", RequestType.GET, null),
     SERVERS_ALL("/servers/all", RequestType.GET, null),
-    VERIFY_WALLET("/verify/wallet", RequestType.POST, ctx -> ctx.result("Not implemented yet.")),
-    VERIFY_DISCORD("/verify/discord", RequestType.POST, ctx -> ctx.result("Not implemented yet.")),
+    VERIFY_WALLET("/verify/wallet", RequestType.POST, new VerifyWallet()),
+    VERIFY_DISCORD("/verify/discord", RequestType.POST, new VerifyDiscord()),
     FORM_STAFF_APPLICATION("/form/staff-application", RequestType.POST, null),
     FORM_PUNISHMENT_APPEAL("/form/punishment-appeal", RequestType.POST, null),
     FORM_REPORT_STAFF_MEMBER("/form/report-staff-member", RequestType.POST, null);
---
+++ MSRestfulWeb/src/main/java/gg/mooncraft/services/restfulweb/endpoints/post/impl/VerifyWallet.java
@@ -0,0 +1,13 @@
+package gg.mooncraft.services.restfulweb.endpoints.post.impl;
+
+import gg.mooncraft.services.restfulweb.endpoints.post.AuthHandler;
+import io.javalin.http.Context;
+import org.jetbrains.annotations.NotNull;
+
+public class VerifyWallet extends AuthHandler {
+    
+    @Override
+    public void handleAuthorized(@NotNull Context ctx) {
+        ctx.result("Not implemented yet.");
+    }
+}
