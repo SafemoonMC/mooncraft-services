011a8578860b932a56b663d5fd20f3ec532f1756
Eduard Wayland <eduproard@gmail.com>
2021-11-23 16:19:46 +0100

    Parsing UUID without exception

--- MSRestfulWeb/src/main/java/gg/mooncraft/services/restfulweb/utilities/StringUtilities.java
+++ MSRestfulWeb/src/main/java/gg/mooncraft/services/restfulweb/utilities/StringUtilities.java
@@ -2,11 +2,23 @@ package gg.mooncraft.services.restfulweb.utilities;
 
 import lombok.AccessLevel;
 import lombok.NoArgsConstructor;
+import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
+
+import java.util.UUID;
 
 @NoArgsConstructor(access = AccessLevel.PRIVATE)
 public final class StringUtilities {
     
     public static boolean hasLetters(String text) {
         return text != null && !text.isEmpty() && !text.isBlank();
+    }
+    
+    public static @Nullable UUID parseUniqueId(@NotNull String literalUniqueId) {
+        try {
+            return UUID.fromString(literalUniqueId);
+        } catch (Exception e) {
+            return null;
+        }
     }
 }
