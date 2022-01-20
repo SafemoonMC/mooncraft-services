46a76b7e36e8ebea8e0787ca9a1551a1da85f8b1
Eduard Wayland <eduproard@gmail.com>
2022-01-20 23:54:47 +0100

    Improved exception handling

--- MSMinecraft/Core/src/gg/mooncraft/services/minecraft/core/JedisManager.java
+++ MSMinecraft/Core/src/gg/mooncraft/services/minecraft/core/JedisManager.java
@@ -1,6 +1,7 @@
 package gg.mooncraft.services.minecraft.core;
 
 import org.jetbrains.annotations.NotNull;
+
 import redis.clients.jedis.HostAndPort;
 import redis.clients.jedis.Jedis;
 import redis.clients.jedis.JedisPool;
@@ -9,19 +10,19 @@ import redis.clients.jedis.JedisPoolConfig;
 import java.util.concurrent.CompletableFuture;
 
 public final class JedisManager {
-    
+
     /*
     Fields
      */
     private final @NotNull JedisPool jedisPool;
-    
+
     /*
     Constructor
      */
     public JedisManager(@NotNull JedisPoolConfig jedisPoolConfig, @NotNull HostAndPort hostAndPort, @NotNull String username, @NotNull String password) {
         this.jedisPool = new JedisPool(jedisPoolConfig, hostAndPort.getHost(), hostAndPort.getPort(), username, password);
     }
-    
+
     /*
     Methods
      */
@@ -31,29 +32,38 @@ public final class JedisManager {
             try (Jedis jedis = jedisPool.getResource()) {
                 jedis.set(key, value);
                 return true;
+            } catch (Exception e) {
+                System.out.println("Error addKeyValue: " + e);
+                return false;
             }
         });
     }
-    
+
     public @NotNull CompletableFuture<Boolean> delKeyValue(@NotNull String key) {
         if (jedisPool.isClosed()) return CompletableFuture.completedFuture(false);
         return CompletableFuture.supplyAsync(() -> {
             try (Jedis jedis = jedisPool.getResource()) {
                 jedis.del(key);
                 return true;
+            } catch (Exception e) {
+                System.out.println("Error delKeyValue: " + e);
+                return false;
             }
         });
     }
-    
+
     public @NotNull CompletableFuture<String> getKeyValue(@NotNull String key) {
         if (jedisPool.isClosed()) return CompletableFuture.completedFuture(null);
         return CompletableFuture.supplyAsync(() -> {
             try (Jedis jedis = jedisPool.getResource()) {
                 return jedis.get(key);
+            } catch (Exception e) {
+                System.out.println("Error getKeyValue: " + e);
+                return null;
             }
         });
     }
-    
+
     public void stop() {
         if (!jedisPool.isClosed()) jedisPool.close();
     }
