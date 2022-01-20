669553d341c226874f550e23b8370bf19fbfe860
Eduard Wayland <eduproard@gmail.com>
2022-01-20 23:56:10 +0100

    Code cleanup

--- MSMinecraft/Bungee/src/gg/mooncraft/services/minecraft/bungee/MSMinecraftMain.java
+++ MSMinecraft/Bungee/src/gg/mooncraft/services/minecraft/bungee/MSMinecraftMain.java
@@ -1,8 +1,24 @@
 package gg.mooncraft.services.minecraft.bungee;
 
+import lombok.AccessLevel;
+import lombok.Getter;
+
 import com.google.gson.FieldNamingPolicy;
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
+
+import me.eduardwayland.mooncraft.waylander.database.Credentials;
+import me.eduardwayland.mooncraft.waylander.database.Database;
+import me.eduardwayland.mooncraft.waylander.database.connection.hikari.impl.MariaDBConnectionFactory;
+import me.eduardwayland.mooncraft.waylander.database.scheme.db.NormalDatabaseScheme;
+import me.eduardwayland.mooncraft.waylander.database.scheme.file.NormalSchemeFile;
+import me.eduardwayland.mooncraft.waylander.scheduler.Scheduler;
+
+import net.md_5.bungee.api.plugin.Plugin;
+import net.md_5.bungee.config.Configuration;
+
+import org.jetbrains.annotations.NotNull;
+
 import gg.mooncraft.services.minecraft.bungee.api.ApiRegistrationUtility;
 import gg.mooncraft.services.minecraft.bungee.api.impl.MSMinecraftImpl;
 import gg.mooncraft.services.minecraft.bungee.commands.IOCommand;
@@ -19,17 +35,6 @@ import gg.mooncraft.services.minecraft.bungee.scheduler.BungeeScheduler;
 import gg.mooncraft.services.minecraft.bungee.utilities.BungeeRedisUtilities;
 import gg.mooncraft.services.minecraft.bungee.utilities.IOUtils;
 import gg.mooncraft.services.minecraft.core.JedisManager;
-import lombok.AccessLevel;
-import lombok.Getter;
-import me.eduardwayland.mooncraft.waylander.database.Credentials;
-import me.eduardwayland.mooncraft.waylander.database.Database;
-import me.eduardwayland.mooncraft.waylander.database.connection.hikari.impl.MariaDBConnectionFactory;
-import me.eduardwayland.mooncraft.waylander.database.scheme.db.NormalDatabaseScheme;
-import me.eduardwayland.mooncraft.waylander.database.scheme.file.NormalSchemeFile;
-import me.eduardwayland.mooncraft.waylander.scheduler.Scheduler;
-import net.md_5.bungee.api.plugin.Plugin;
-import net.md_5.bungee.config.Configuration;
-import org.jetbrains.annotations.NotNull;
 import redis.clients.jedis.HostAndPort;
 import redis.clients.jedis.JedisPoolConfig;
 
@@ -40,28 +45,28 @@ import java.io.InputStream;
 
 @Getter
 public class MSMinecraftMain extends Plugin {
-    
+
     /*
     Fields
      */
     private static MSMinecraftMain instance;
     private static Gson gson;
-    
+
     private BungeeConfiguration bungeeConfiguration;
     private ConfigurationPrefabs configurationPrefabs;
-    
+
     private Database database;
     private Scheduler scheduler;
     private JedisManager jedisManager;
-    
+
     private BungeeMessaging bungeeMessaging;
-    
+
     @Getter(value = AccessLevel.NONE)
     private MSMinecraftImpl msmBungeeImpl;
     private NetworkPlayersFactory networkPlayersFactory;
     private NetworkServersFactory networkServersFactory;
     private NetworkCountersFactory networkCountersFactory;
-    
+
     /*
     Override Methods
      */
@@ -70,15 +75,15 @@ public class MSMinecraftMain extends Plugin {
         // Load instances
         instance = this;
         gson = new GsonBuilder().setFieldNamingPolicy(FieldNamingPolicy.LOWER_CASE_WITH_DASHES).create();
-        
+
         // Load configuration
         this.bungeeConfiguration = new BungeeConfiguration(this);
         this.configurationPrefabs = new ConfigurationPrefabs(this.bungeeConfiguration);
-        
+
         // Load Waylander libraries and JedisManager
         try {
             this.scheduler = new BungeeScheduler();
-            
+
             if (!loadDatabase()) {
                 getLogger().warning("Database connection cannot be estabilished.");
                 return;
@@ -92,50 +97,50 @@ public class MSMinecraftMain extends Plugin {
             onDisable();
             return;
         }
-        
+
         // Load BungeeMessaging
         this.bungeeMessaging = new BungeeMessaging();
-        
+
         // Load public API
         this.networkPlayersFactory = new NetworkPlayersFactory();
         this.networkCountersFactory = new NetworkCountersFactory();
         this.networkServersFactory = new NetworkServersFactory();
-        
+
         this.msmBungeeImpl = new MSMinecraftImpl(this.networkPlayersFactory, this.networkServersFactory, this.networkCountersFactory);
         ApiRegistrationUtility.registerProvider(this.msmBungeeImpl);
-        
+
         // Load listeners
         new ServerListeners();
         new CommunityListeners();
-        
+
         // Load commands
         new VerifyCommand();
         getConfigurationPrefabs().getCommunityInputOutputPrefab().getInputOutputList().forEach(inputOutput -> new IOCommand(inputOutput.command(), inputOutput.permission()));
-        
+
         getLogger().info("Enabled!");
     }
-    
+
     @Override
     public void onDisable() {
         if (this.jedisManager != null) this.jedisManager.stop();
         ApiRegistrationUtility.unregisterProvider();
-        
+
         if (this.bungeeMessaging != null) this.bungeeMessaging.unload();
-        
+
         getLogger().info("Disabled!");
     }
-    
+
     /*
     Static Methods
      */
     public static @NotNull MSMinecraftMain getInstance() {
         return instance;
     }
-    
+
     public static @NotNull Gson getGson() {
         return gson;
     }
-    
+
     /*
     Methods
      */
@@ -145,39 +150,39 @@ public class MSMinecraftMain extends Plugin {
             getLogger().warning("mysql section is not into the config.yml!");
             return false;
         }
-        
+
         // Load input stream
         InputStream inputStream = getResourceAsStream("msminecraft-db.scheme");
         if (inputStream == null) {
             getLogger().info("msminecraft-db.scheme is not inside the jar.");
             return false;
         }
-        
+
         // Create temporary file
         File temporaryFile = new File(getDataFolder(), "msminecraft-db.scheme");
         if (!temporaryFile.exists() && !temporaryFile.createNewFile()) {
             getLogger().info("The temporary file msminecraft-db.scheme cannot be created.");
             return false;
         }
-        
+
         // Load output stream
         FileOutputStream outputStream = new FileOutputStream(temporaryFile);
-        
+
         // Copy input to output
         IOUtils.copy(inputStream, outputStream);
-        
+
         // Close streams
         inputStream.close();
         outputStream.close();
-        
+
         // Parse database scheme and delete temporary file
         NormalDatabaseScheme normalDatabaseScheme = new NormalSchemeFile(temporaryFile).parse();
         if (!temporaryFile.delete()) {
             getLogger().info("The temporary file msminecraft-db.scheme cannot be deleted. You could ignore this warning!");
         }
-        
+
         Credentials credentials = BungeeDatabaseUtilities.fromBukkitConfig(configuration.getSection("mysql"));
-        
+
         // Setup database
         this.database = Database.builder()
                 .identifier(getDescription().getName())
@@ -188,11 +193,12 @@ public class MSMinecraftMain extends Plugin {
                 .build();
         return true;
     }
-    
+
     boolean loadJedisManager() {
         try {
             Configuration sectionRedis = getBungeeConfiguration().getConfiguration().getSection("redis");
-            if (sectionRedis == null) throw new IllegalStateException("redis section is missing from config.yml");
+            if (sectionRedis == null)
+                throw new IllegalStateException("redis section is missing from config.yml");
             HostAndPort hostAndPort = BungeeRedisUtilities.parseHostAndPort(sectionRedis);
             String username = sectionRedis.getString("username");
             String password = sectionRedis.getString("password");
@@ -202,7 +208,7 @@ public class MSMinecraftMain extends Plugin {
             if (sectionPoolSettings == null)
                 throw new IllegalStateException("pool-settings section is missing from redis configuration section");
             JedisPoolConfig jedisPoolConfig = BungeeRedisUtilities.parsePoolConfig(sectionPoolSettings);
-            
+
             this.jedisManager = new JedisManager(jedisPoolConfig, hostAndPort, username, password);
         } catch (Exception e) {
             getLogger().warning(e.getMessage());
--- MSMinecraft/Bungee/src/gg/mooncraft/services/minecraft/bungee/factories/NetworkServersFactory.java
+++ MSMinecraft/Bungee/src/gg/mooncraft/services/minecraft/bungee/factories/NetworkServersFactory.java
@@ -1,13 +1,15 @@
 package gg.mooncraft.services.minecraft.bungee.factories;
 
+import lombok.Getter;
+
+import net.md_5.bungee.api.ProxyServer;
+
+import org.jetbrains.annotations.NotNull;
 
 import gg.mooncraft.services.datamodels.NetworkServers;
 import gg.mooncraft.services.datamodels.ProtocolKeys;
 import gg.mooncraft.services.minecraft.bungee.MSMinecraftMain;
 import gg.mooncraft.services.minecraft.bungee.config.prefabs.ServersDisplayPrefab;
-import lombok.Getter;
-import net.md_5.bungee.api.ProxyServer;
-import org.jetbrains.annotations.NotNull;
 
 import java.util.HashMap;
 import java.util.List;
@@ -18,19 +20,19 @@ import java.util.stream.Collectors;
 
 @Getter
 public final class NetworkServersFactory {
-    
+
     /*
     Fields
      */
     private @NotNull NetworkServers networkServers;
-    
+
     /*
     Constructor
      */
     public NetworkServersFactory() {
         this.networkServers = update().join();
     }
-    
+
     /*
     Methods
      */
@@ -38,16 +40,16 @@ public final class NetworkServersFactory {
         return CompletableFuture.supplyAsync(() -> {
                     Set<String> serverList = ProxyServer.getInstance().getServers().keySet();
                     List<String> groupsList = serverList.stream().map(serverName -> serverName.contains("-") ? serverName.split("-")[0] : serverName).collect(Collectors.toList());
-                    
+
                     Map<String, NetworkServers.ServerGroup> map = new HashMap<>();
                     for (String groupName : groupsList) {
                         String display = MSMinecraftMain.getInstance().getConfigurationPrefabs().getServersDisplayPrefab().getServerDisplay(groupName).map(ServersDisplayPrefab.ServerDisplay::display).orElse(groupName);
                         List<String> servers = serverList.stream().filter(serverName -> serverName.toLowerCase().startsWith(groupName)).collect(Collectors.toList());
-                        
+
                         NetworkServers.ServerGroup serverGroup = new NetworkServers.ServerGroup(display, servers);
                         map.put(groupName, serverGroup);
                     }
-                    
+
                     return new NetworkServers(map);
                 })
                 .thenApply(newNetworkServers -> this.networkServers = newNetworkServers)
